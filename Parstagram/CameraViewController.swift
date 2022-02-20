4408aefabe525dbc91780a5ada8e50f1b2759eff
niantony <66891025+niantony@users.noreply.github.com>
2022-02-20 17:24:58 -0500

    Update

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -10,9 +10,11 @@ import AlamofireImage
 import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     @IBOutlet weak var imageView: UIImageView!
+    
     @IBOutlet weak var commentField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,23 +23,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     @IBAction func onSubmitButton(_ sender: Any) {
         let post = PFObject(className: "Posts")
-        
-        post["caption"] = commentField.text
+        post["caption"] = commentField.text!
         post["author"] = PFUser.current()!
         
         let imageData = imageView.image!.pngData()
         let file = PFFileObject(data: imageData!)
-        
         post["image"] = file
         
-        post.saveInBackground{ (success, error) in
-            if (success) {
+        post.saveInBackground {
+            (success, error) in
+            if success {
                 self.dismiss(animated: true, completion: nil)
                 print("saved")
             } else {
                 print("error")
             }
         }
+        
+        
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
@@ -55,16 +59,16 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            let image = info[.editedImage] as! UIImage
-            let size = CGSize(width: 300, height: 300)
-            let scaledImage = image.af.imageScaled(to: size)
-            imageView.image = scaledImage
-            
-            dismiss(animated: true, completion: nil)
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
+        
+        
     }
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
